2d6036267ee33ace8ed46adf29724b6fe27c3a2a
jabinich <jabinich@gmail.com>
2023-05-11 15:21:32 +0200

    routing and route with parameter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { APP_ROUTES } from './app.routes';
 import { FlightBookingModule } from './flight-booking/flight-booking.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
    imports: [
+      RouterModule.forRoot(APP_ROUTES, { enableTracing: true }),
       HttpClientModule,
       BrowserModule,
       FlightBookingModule
@@ -15,7 +21,10 @@ import { AppComponent } from './app.component';
    declarations: [
       AppComponent,
       SidebarComponent,
-      NavbarComponent
+      NavbarComponent,
+      HomeComponent,
+      AboutComponent,
+      NotFoundComponent
    ],
    providers: [],
    bootstrap: [
--- src/app/flight-booking/flight-booking.module.ts
+++ src/app/flight-booking/flight-booking.module.ts
@@ -3,15 +3,22 @@ import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { FlightSearchComponent } from './flight-search/flight-search.component';
 import { FlightCardComponent } from './flight-card/flight-card.component';
+import { PassengerSearchComponent } from './passenger-search/passenger-search.component';
+import { FLIGHT_BOOKING_ROUTES } from './flight-booking.routes';
+import { RouterModule } from '@angular/router';
+import { FlightEditComponent } from './flight-edit/flight-edit.component';
 
 @NgModule({
   imports: [
+    RouterModule.forChild(FLIGHT_BOOKING_ROUTES),
     FormsModule,
     SharedModule
   ],
   declarations: [
     FlightSearchComponent,
-    FlightCardComponent
+    FlightCardComponent,
+    PassengerSearchComponent,
+    FlightEditComponent
   ],
   exports: [
     FlightSearchComponent
